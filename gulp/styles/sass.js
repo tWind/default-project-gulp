ee97416556b93f374efa35227b8408cb8f53e50e
tWind <su.ilveran@gmail.com>
2017-02-25 13:50:45 +0300

    gulp sass fix

--- gulp/styles/sass.js
+++ gulp/styles/sass.js
@@ -1,12 +1,13 @@
 module.exports = function() {
     var gulp = this.gulp;
+    var project = this.opts.project;
     
     var sass = require('gulp-sass');
     var sourcemaps = require('gulp-sourcemaps');
-    
-    return gulp.src('./public/*.scss')
+
+    return gulp.src(project.app + '/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public/dist/css/'));
+        .pipe(gulp.dest(project.app + '/dist/css/'));
 };
