5603e9fceb9430b8e607c265d54a79a37579e52e
tWind <su.ilveran@gmail.com>
2017-04-12 11:41:36 +0300

    watch scripts task modify

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,8 +2,13 @@ module.exports = function() {
     var gulp = this.gulp;
     var project = this.opts.project;
 
-    gulp.watch(project.app + '/**/*.scss', ['styles:sass']); 
-    gulp.watch(project.blocks + '/**/*.js', ['scripts:run']);
+    gulp.watch(project.app + '/**/*.scss', ['styles:sass']);
+    gulp.watch(
+        [
+            project.blocks + '/**/*.js',
+            project.app + '/js/*.js'
+        ],
+        ['scripts:run']);
     gulp.watch([project.templates + '/*.html', project.blocks + '/**/*.html'], ['html:build']);
 
     // Globs beginning with "./" doesn't trigger file events :cc
